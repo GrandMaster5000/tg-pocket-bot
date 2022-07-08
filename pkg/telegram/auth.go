866c1cb4418888a8e781048f80949335371eb5b1
tolik22869 <ggamer556@mail.ru>
2022-07-08 14:00:55 +0300

    upgrade handlers

--- pkg/telegram/auth.go
+++ pkg/telegram/auth.go
@@ -5,7 +5,24 @@ import (
 	"fmt"
 
 	"github.com/GrandMaster5000/tg-bot-pocket/pkg/repository"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+func (b *Bot) initAuthorizationProccess(message *tgbotapi.Message) error {
+	authLink, err := b.generateAuthorizationLink(message.Chat.ID)
+	if err != nil {
+		return err
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, fmt.Sprintf(replyStartTemplate, authLink))
+
+	_, err = b.bot.Send(msg)
+	return err
+}
+
+func (b *Bot) getAccessToken(chatID int64) (string, error) {
+	return b.tokenRepository.Get(chatID, repository.AccessToken)
+}
 
 func (b *Bot) generateAuthorizationLink(chatID int64) (string, error) {
 	redirectURL := b.generateRedirectURL(chatID)
--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -1,16 +1,22 @@
 package telegram
 
 import (
-	"fmt"
-	"log"
+	"context"
+	"net/url"
 
+	"github.com/GrandMaster5000/go-pocket-sdk"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 const (
 	commandStart = "start"
 
-	replyStartTemplate = "Привет! Чтобы сохранить ссылки в своём Pocket аккаунте, для начала тебе необходимо дать мне на это доступ. Для этого переходи по ссылке:\n%s"
+	replyStartTemplate     = "Привет! Чтобы сохранить ссылки в своём Pocket аккаунте, для начала тебе необходимо дать мне на это доступ. Для этого переходи по ссылке:\n%s"
+	replyAlreadyAuthorized = "Ты уже авторизирован. Присылай ссылку, а я её сохраню."
+	replyLinkSuccessSave   = "Ссылка успешно сохранена."
+	replyNotValidLink      = "Это невалидная ссылка."
+	replyYouNotAuthorized  = "Ты не авторизирован. Используй команду /start."
+	replyFaildSaveLink     = "Увы, не удалось сохранить ссылку. Попробуй ещё раз позже."
 )
 
 func (b *Bot) handleCommand(message *tgbotapi.Message) error {
@@ -22,24 +28,46 @@ func (b *Bot) handleCommand(message *tgbotapi.Message) error {
 	}
 }
 
-func (b *Bot) handleMessage(message *tgbotapi.Message) {
-	log.Printf("[%s] %s", message.From.UserName, message.Text)
+func (b *Bot) handleMessage(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, replyLinkSuccessSave)
 
-	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
-	msg.ReplyToMessageID = message.MessageID
-
-	b.bot.Send(msg)
-}
-
-func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
-	authLink, err := b.generateAuthorizationLink(message.Chat.ID)
+	_, err := url.ParseRequestURI(message.Text)
 	if err != nil {
+		msg.Text = replyNotValidLink
+		_, err = b.bot.Send(msg)
 		return err
 	}
 
-	msg := tgbotapi.NewMessage(message.Chat.ID, fmt.Sprintf(replyStartTemplate, authLink))
+	accessToken, err := b.getAccessToken(message.Chat.ID)
+	if err != nil {
+		msg.Text = replyYouNotAuthorized
+		_, err = b.bot.Send(msg)
+		return err
+	}
+
+	if err = b.pocketClient.Add(context.Background(), pocket.AddInput{
+		AccessToken: accessToken,
+		URL:         message.Text,
+	}); err != nil {
+		msg.Text = replyFaildSaveLink
+		_, err = b.bot.Send(msg)
+		return err
+	}
 
 	_, err = b.bot.Send(msg)
+
+	return err
+}
+
+func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
+	_, err := b.getAccessToken(message.Chat.ID)
+	if err != nil {
+		return b.initAuthorizationProccess(message)
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, replyAlreadyAuthorized)
+	_, err = b.bot.Send(msg)
+
 	return err
 }
 
